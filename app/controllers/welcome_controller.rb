8b81e44c14fe19bf65e6066bcfc7c0b8030a2358
vankizzle <you@example.com>
2017-10-09 11:46:01 -0700

    asd

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,15 @@
 class WelcomeController < ApplicationController
-  def index
-  end
+require'csv'
+ protect_from_forgery with :null_sessions 
+  
+def sum 
+	sum = 0
+
+		CSV.foreach(params[:file].read) do |row|
+	
+		sum+=row[0].to_i
+
+		end
+	render plain: "%.2f" % (sum).ceil;
+end
 end
